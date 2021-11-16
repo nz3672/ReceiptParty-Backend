8de0200c947cea4d376972828db226ab7596a831
telio-s <telio.s@hotmail.com>
2021-11-16 12:21:35 +0700

    first commit

---
+++ src/main/java/com/receiptparty/ReceiptParty/ReceiptPartyApplication.java
@@ -0,0 +1,13 @@
+package com.receiptparty.ReceiptParty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptPartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/receiptparty/ReceiptParty/ReceiptPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receiptparty.ReceiptParty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
